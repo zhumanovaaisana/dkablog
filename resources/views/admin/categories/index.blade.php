490d82d2a950476ec103a620fe96086bdebc20c9
zhumanovaaisana <zhumanova.aisana@gmail.com>
2019-12-09 16:55:26 +0600

    Create category.indec page

---
+++ resources/views/admin/categories/index.blade.php
@@ -0,0 +1,51 @@
+@extends('admin.layouts.app_admin')
+
+@section('content')
+
+<div class="container">
+
+	@component('admin.components.breadcrumb')
+	  @slot('title') Список категорий @endslot
+	  @slot('parent') Главная @endslot
+	  @slot('active') Категории @endslot
+	@endcomponent
+	
+	<hr>
+
+	<a href="{{route('admin.category.create')}}" class="btn btn-primary pull-right"><i class="fafa-plus-square-o"></i>Создать категорию</a>
+	<table class="table table-striped">
+		<thead>
+			<th>Наименование</th>
+			<th>Публикация</th>
+			<th class="text-right">Действие</th>
+		</thead>
+		<tbody>
+			@forelse ($categories as $category)
+			  <tr>
+			  	<td>{{$category->title}}</td>
+			  	<td>{{$category->published}}</td>
+			  	<td>
+			  		<a href="{{route('admin.category.edit'.['id'=>$category->id])}}"><i class="fafa-edit"></i></a>
+			  	</td>
+			  </tr>
+			  @empty
+			  <tr>
+			  	<td colspan="3" class="text-container"><h2>Данные не существуют</h2></td>
+			  </tr>
+			  @endforelse
+		</tbody>
+		<tfoot>
+			<tr>
+				<td colspan="3">
+					<ul class="pagination pull-right">
+						{{$categories -> links()}}
+					</ul>
+				</td>
+			</tr>
+		</tfoot>
+	</table>
+
+
+</div>
+
+@endsection
--- resources/views/admin/layouts/app_admin.blade.php
+++ resources/views/admin/layouts/app_admin.blade.php
@@ -18,6 +18,9 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
     <div id="app">
@@ -36,7 +39,7 @@
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Блог</a>
                             <ul class="dropdown-menu" role="menu">
-                                <li><a href="#">Категории</a></li>
+                                <li><a href="{{route('admin.category.index')}}">Категории</a></li>
                                 <li><a href="#">Материалы</a></li>
                             </ul>
 
